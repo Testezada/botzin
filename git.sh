a8787ccbedcf36461e8c31d45d301d1f1221267b
testevrzn <78117358+testevrzn@users.noreply.github.com>
2021-02-26 16:28:22 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -77,8 +77,8 @@ cd $HOME
 pkg update && pkg upgrade
 pkg install git
 pkg install wget
-git clone https://github.com/jajalhd/teste1
-cd teste1
+git clone https://github.com/jajalhd/kp
+cd kp
 pkg install unzip
 pkg install nodejs
 pkg inatall ffmpeg
