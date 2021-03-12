de7f0780fcfdce3f4f0b8db8e2aaf63f2cce74a4
Testezada <80421442+Testezada@users.noreply.github.com>
2021-03-12 13:26:56 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -77,14 +77,8 @@ cd $HOME
 pkg update && pkg upgrade
 pkg install git
 pkg install wget
-git clone https://github.com/jajalhd/kp
-cd kp
-pkg install unzip
-pkg install nodejs
-pkg inatall ffmpeg
-pkg install tesseract
-unzip w.zip
-cd w
+git clone https://github.com/testevrzn/OFF
+cd OFF
 bash install.sh
 sleep 2
 echo ""
@@ -94,6 +88,4 @@ sleep 1
 echo "Prepare 2 telefones celulares para suprimir o QR!!"
 sleep 2
 echo "carga......."
-sleep 7
-node index.js
 fi
